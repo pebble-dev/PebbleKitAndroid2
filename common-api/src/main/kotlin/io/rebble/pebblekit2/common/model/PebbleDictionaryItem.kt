6952d3078ad2d4a1c6a83ccf8b10cca83eb40669
Matej Drobnič <matej@matejdro.com>
2025-12-03 17:41:19 +0100

    fix(common-api): equality checks for the PebbleDictionaryItem.ByteArray

--- common-api/src/main/kotlin/io/rebble/pebblekit2/common/model/PebbleDictionaryItem.kt
+++ common-api/src/main/kotlin/io/rebble/pebblekit2/common/model/PebbleDictionaryItem.kt
@@ -3,7 +3,20 @@ package io.rebble.pebblekit2.common.model
 public sealed class PebbleDictionaryItem {
     public abstract val value: Any
 
-    public data class ByteArray(override val value: kotlin.ByteArray) : PebbleDictionaryItem()
+   public data class ByteArray(override val value: kotlin.ByteArray) : PebbleDictionaryItem() {
+      override fun equals(other: Any?): Boolean {
+         if (this === other) return true
+         if (other !is ByteArray) return false
+
+         if (!value.contentEquals(other.value)) return false
+
+         return true
+      }
+
+      override fun hashCode(): Int {
+         return value.contentHashCode()
+      }
+   }
 
     public data class String(override val value: kotlin.String) : PebbleDictionaryItem()
 
